65c0499f6a16b69630d2872c7bbd296f0fffbe3f
ziarekk <karol.ziarek@pw.edu.pl>
2024-04-04 23:28:07 +0200

    bait crafiting

--- prolog/main.pl
+++ prolog/main.pl
@@ -69,6 +69,24 @@ use_item(bestiariusz) :-
     write('- Gryf is a fearsome creature that dwells in dark caves.'), nl,
     write('- It is vulnerable to light-based attacks.'), nl.
 
+% Złożenie pzynęty na gryfa
+craft_bait() :-
+    % Sprawdzenie, czy gracz ma odpowiednią ilość każdego składnika
+    inventory(Inventory),
+    member((siarka, 1), Inventory),
+    member((ziele_szatana, 1), Inventory),
+    member((krowia_skora, 2), Inventory),
+    % Usunięcie składników z ekwipunku
+    remove_from_inventory(siarka, 1),
+    remove_from_inventory(ziele_szatana, 1),
+    remove_from_inventory(krowia_skora, 2),
+    add_to_inventory(przynęta_na_gryfa, 1),
+    write('Udało Ci się wytworzyć przynętę na gryfa'), nl.
+
+% Informacja, gdy gracz nie ma niezbędnych składników
+craft_bait :-
+    write('Nie masz przedmiotów niezbędnych do wytworzenia przynęty. \n Do wytworzenia przynęty niezbędne są: \n- 2 krowie skóry \n- siarka \n-ziele szatana'), nl.
+
 start :-
     assert(i_am_at(c3)),
     assert(inventory([(bestiariusz, 1)])),
