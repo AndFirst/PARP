4f3752ccc73b1aced3ddfe7161553ba4849830eb
ziarekk <karol.ziarek@pw.edu.pl>
2024-04-04 21:12:45 +0200

    Items use: bestiariusz

--- prolog/main.pl
+++ prolog/main.pl
@@ -6,41 +6,70 @@
 
 
 % Dodawanie przedmiotu do ekwipunku
-add_to_inventory(Item) :-
+add_to_inventory(Item, Quantity) :-
     retract(inventory(Inventory)),
-    assert(inventory([Item|Inventory])).
+    % Sprawdzenie, czy przedmiot już istnieje w ekwipunku
+    (   
+        member((Item, OldQuantity), Inventory) ->
+        NewQuantity is OldQuantity + Quantity,
+        select((Item, OldQuantity), Inventory, TempInventory),
+        assert(inventory([(Item, NewQuantity)|TempInventory]))
+        ; 
+        assert(inventory([(Item, Quantity)|Inventory]))
+    ).
 
 % Usuwanie przedmiotu z ekwipunku
-remove_from_inventory(Item) :-
+remove_from_inventory(Item, Quantity) :-
     retract(inventory(Inventory)),
-    select(Item, Inventory, NewInventory),
-    assert(inventory(NewInventory)).
+    % Sprawdzenie, czy przedmiot istnieje w ekwipunku
+    (   
+        member((Item, OldQuantity), Inventory),
+        OldQuantity >= Quantity ->
+        NewQuantity is OldQuantity - Quantity,
+        select((Item, OldQuantity), Inventory, TempInventory),
+        % Usunięcie przedmiotu z ekwipunku, jeśli jego ilość wynosi 0
+        (   NewQuantity =:= 0 -> 
+            assert(inventory(TempInventory))
+            ;
+            assert(inventory([(Item, NewQuantity)|TempInventory]))
+        )
+        ; 
+        % Przedmiot nie występuje w ekwipunku lub ilość jest niewystarczająca
+        assert(inventory(Inventory))
+    ).
 
 display_inventory :-
     inventory(Inventory),
-    write('W twoim ekwipunku znajdują się: '), nl,
+    write('Your inventory:'), nl,
     display_items(Inventory).
 
 % Wyświetlanie przedmiotów w ekwipunku
 display_items([]).
-display_items([Item|Rest]) :-
-    write('- '), write(Item), nl,
+display_items([(Item, Quantity)|Rest]) :-
+    write('- '), write(Item), write_quantity(Quantity), nl,
     display_items(Rest).
 
-% Predykat obsługujący użycie przedmiotów
-use(Item) :-
-    % Sprawdzenie czy przedmiot znajduje się w ekwipunku
-    inventory(Inventory),
-    member(Item, Inventory),
-    % Obsługa różnych przedmiotów
-    (
-        Item = bestiariusz -> write('Bestiariusz Wiedźminski, Kaer Morhen 1261 \n (...) \n Jak zabic gryfa.txt \n')  
-        % Tutaj możesz dodać obsługę innych przedmiotów
-        ; write('You cannot use that item.')
-    ), nl.
+% Wyświetlanie ilości przedmiotu, jeśli jest większa niż 1
+write_quantity(1) :- !.
+write_quantity(Quantity) :-
+    write(' (x'), write(Quantity), write(')').
 
+% Użycie przedmiotu z ekwipunku
+use(Item) :-
+    % Sprawdzenie, czy przedmiot istnieje w ekwipunku
+    inventory(Inventory),
+    member((Item, _), Inventory),
+    % Wykonanie akcji związanej z użyciem przedmiotu
+    use_item(Item),
+    nl.
+
+% Implementacja akcji związanej z użyciem przedmiotu (przykład)
+use_item(bestiariusz) :-
+    write('You open the bestiary and find information about Gryf:'), nl,
+    write('- Gryf is a fearsome creature that dwells in dark caves.'), nl,
+    write('- It is vulnerable to light-based attacks.'), nl.
 
 start :-
     assert(i_am_at(c3)),
-    assert(inventory([bestiariusz])),
+    assert(inventory([(bestiariusz, 1)])),
     intro.
