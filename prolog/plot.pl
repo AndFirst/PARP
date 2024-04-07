c14009149cc9d45313cb1715eef3077c6723aec7
jkierucz <01169183@pw.edu.pl>
2024-04-07 21:35:58 +0200

    completed instructions message

--- prolog/plot.pl
+++ prolog/plot.pl
@@ -10,12 +10,25 @@ instructions :-
     write('Wprowadź zaklęcia w Starszej Mowie (standardowej składni Prologa).'), nl,
     write('Dostępne zaklęcia:'), nl,
     write('n.  s.  e.  w.     -- aby przejść w danym kierunku.'), nl,
+    wrtie('Go(kierunek).     -- aby przejść w danym kierunku.'), nl,
     write('look.              -- aby ponownie rozejrzeć się obok siebie.'), nl,
     write('look(kierunek).    -- aby spojrzeć w jednym z czterech kierunków.'), nl,
     write('enter(miejsce).    -- aby wejść przez drzwi do spejalnych lokacji'), nl, 
     write('instructions.      -- aby ponownie zobaczyć tę wiadomość.'), nl,
     write('halt.              -- aby zakończyć grę i wyjść.'), nl,
-    nl.
+    write('Bestiariusz.       -- aby dowiedzieć się więcej o potworach.'), nl,
+    write('monety.            -- aby sprawdzić ilość monet.'), nl,
+    write('display_inventory. -- aby zobaczyć zawartość ekwipunku.'), nl,
+    write('podnies(przedmiot). -- aby podnieść przedmiot.'), nl,
+    write('use(przedmiot).    -- aby użyć przedmiotu.'), nl,
+    write('interact(postać). -- aby porozmawiać z postacią.'), nl,
+    write('craft_bait.      -- aby stworzyć przynętę na gryfa.'), nl,
+    write('-----------------------------------------------------------------'), nl.
+    write('W wieży Babel:'), nl,
+    write('enter. -- aby wejść do wieży.'), nl,
+    write('exit.  -- aby wyjść z wieży.'), nl,
+    write('aard.  -- aby użyć znaku Aard.'), nl.
+
 
 % Implementacja akcji związanej z użyciem przedmiotu (przykład)
 bestiariusz :-
